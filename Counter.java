7fda0d29526cd14018d9b4eb6bf595eed7442cf4
Sam.Higgs <higgssam0@gmail.com>
2021-11-27 10:33:45 +0000

    changing piece to counter

--- Counter.java
+++ Counter.java
@@ -1,14 +1,14 @@
-public class Piece {
+public class Counter {
 
-    // field - Initialise the colour of the piece.
+    // field - Initialise the colour of the counter.
     private String colour;
 
-    // constructor - setting the colour of a piece.
+    // constructor - setting the colour of a counter.
     public void setColour(String colour) {
         this.colour = colour;
     }
 
-    // accessor - getting the colour of a piece.
+    // accessor - getting the colour of a counter.
     public String getColour() {
         return colour;
     }
